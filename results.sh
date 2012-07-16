49c33cc9e8172ab8adcf9515b4854fd90aa3f8a7
Ubuntu <ubuntu@domU-12-31-39-02-7C-A7.compute-1.internal>
2012-07-16 09:28:27 +0000

    Add results script.

--- results.sh
+++ results.sh
@@ -12,8 +12,10 @@ mysql -u crashncompile -p$PASS crashncompile -e 'select email from submissions j
 
 echo 
 echo "Attempted qualification problem"
-mysql -u crashncompile -p$PASS crashncompile -e 'select email from submissions join users on userid = users.id where problem = 1 group by email;'
+#mysql -u crashncompile -p$PASS crashncompile -e 'select email from submissions join users on userid = users.id where problem = 1 group by email;'
+mysql -u crashncompile -p$PASS crashncompile -e 'select email, start from users where start is not null order by email;' | perl -p -e 's/(\d{10})/localtime($1)/ge'
+date
 
 echo
 echo "Passed qualification problem"
-mysql -u crashncompile -p$PASS crashncompile -e 'select email from submissions join users on userid = users.id where problem = 1 and result = 1 group by email;'
+mysql -u crashncompile -p$PASS crashncompile -e 'select email, submissions.time - users.start from submissions join users on userid = users.id where problem = 1 and result = 1 group by email;'
