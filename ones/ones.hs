37b265dfb9c4b164979437e4b61f2bc940458295
trainman419 <legotown@aol.com>
2012-07-25 16:11:46 -0400

    Add haskell implementation of ones.

---
+++ ones/ones.hs
@@ -0,0 +1,21 @@
+
+-- ones.hs
+
+import System.Environment
+import Data.Digits
+
+
+ones :: Int -> Int
+ones n = foldr (\x y -> if x == 1 then x+y else y) 0 (digits 10 n)
+
+countOnes :: Int -> Int
+countOnes n = foldr (\x y -> (ones x) + y) 0 [0..n]
+
+--  or less efficiently, but still correct
+-- countOnes n = sum $ map ones [0..n]
+
+
+main = do
+  xs <- getArgs
+  let arg' = read (head xs) :: Int
+  print $ countOnes arg'
