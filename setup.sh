b2316bf97061ce6a7aa8d64f14b7f4ce73bc4db5
Austin Hendrix <legotown@aol.com>
2012-08-03 20:32:44 -0700

    First draft of setup script. Untested.

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+prefix=/opt/crashandcompile
+user=www-data
+
+mkdir -p $prefix/archive
+mkdir -p $prefix/tmp
+mkdir -p $prefix/problems
+mkdir -p $prefix/grader
+
+local=`dirname $0`
+echo $local
+
+if [ -d $local/.git ] 
+then
+   echo "Copy files to grader"
+   cp -r $local $prefix/grader/
+else
+   push $prefix/grader
+   git clone https://github.com/trainman419/cnc-grader.git
+fi
+
+chown -R $user $prefix
